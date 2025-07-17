ca911b7b15d34a3169357bd8fd4ce5190c1ecce6
Irina Saf <132776531+rabbitrunout@users.noreply.github.com>
2025-07-17 08:55:21 -0400

    Completed About page

--- bookingsystem/src/app/about/about.ts
+++ bookingsystem/src/app/about/about.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  standalone: true,
   selector: 'app-about',
-  imports: [CommonModule, RouterModule],
+  standalone: true,
+  imports: [ RouterModule],
   templateUrl: './about.html',
   styleUrls: ['./about.css']
 })
--- bookingsystem/src/app/app.routes.ts
+++ bookingsystem/src/app/app.routes.ts
@@ -3,7 +3,9 @@ import { BookingComponent } from './bookingcomponent/booking.component';
 import { About } from './about/about'; // Класс About
 
 export const routes: Routes = [
-  { path: '', component: BookingComponent },
+  { path: 'booking', component: BookingComponent },
+  // { path: 'add', component: AddReservationComponent },
   { path: 'about', component: About },
-  { path: "**", redirectTo: "/bookingcomponent"}
+  { path: '', redirectTo: '/booking', pathMatch: 'full' },  // редирект на booking
+  { path: '**', redirectTo: '/booking' }  // catch-all
 ];
